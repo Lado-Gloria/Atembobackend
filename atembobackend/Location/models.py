1db0f8a12f2604661e2278a98c2e1f98bb625add
Lado-Gloria <glorialadoo@gmail.com>
2023-09-07 21:49:41 +0300

    created location model

---
+++ atembobackend/Location/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Location(models.Model):
+    region_name = models.CharField(max_length=255)
+    installation_date = models.DateField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.region_name
+
+  

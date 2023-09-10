1302e48abfd22f361bab509c24a30aa05cd03e0e
kivuvarosekivuvan <rosekivuvan@gmail..com>
2023-09-10 23:21:35 +0300

    Updated function views for api

--- atembobackend/api/views.py
+++ atembobackend/api/views.py
@@ -88,28 +88,6 @@ class FlowrateDetailView(APIView):
         except Exception as e:
             return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    def put(self, request, id, format=None):
-        try:
-            flow = FlowRate.objects.get(id=id)
-            serializer = FlowRateSerializer(flow, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except FlowRate.DoesNotExist:
-            return Response("FlowRate not found", status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    def delete(self, request, id, format=None):
-        try:
-            flow = FlowRate.objects.get(id=id)
-            flow.delete()
-            return Response("FlowRate successfully deleted", status=status.HTTP_204_NO_CONTENT)
-        except FlowRate.DoesNotExist:
-            return Response("FlowRate not found", status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)    
 
 
 

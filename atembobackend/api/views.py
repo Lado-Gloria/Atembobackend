0b2e5176889047a7887dcc0f956c3d23963046a2
Student <ochiengpakoth@gmail.com>
2023-09-11 11:45:06 +0300

    added api's for registration model

--- atembobackend/api/views.py
+++ atembobackend/api/views.py
@@ -11,24 +11,27 @@ class RegistrationListView(APIView):
         registrations = Registration.objects.all()
         serializer = RegistrationSerializer(registrations, many=True)
         return Response(serializer.data)
+    
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 class RegistrationDetailView(APIView):
     def get(self, request, id, format=None):
         registration = Registration.objects.get(id=id)
         serializer = RegistrationSerializer(registration)
         return Response(serializer.data)
+    
     def put(self, request, id, format=None):
         registration = Registration.objects.get(id=id)
         serializer = RegistrationSerializer(registration, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
     def delete(self, request, id, format=None):
         registration = Registration.objects.get(id=id)
         registration.delete()

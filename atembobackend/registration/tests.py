5fb17bba0d3cfd037805c49d601aafe313236989
Student <ochiengpakoth@gmail.com>
2023-09-12 15:57:36 +0300

    added unit testing

--- atembobackend/registration/tests.py
+++ atembobackend/registration/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import Registration
+
 
 # Create your tests here.
+class RegistrationModelTest(TestCase):
+
+    def test_create_registration(self):
+        registration = Registration.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="johndoe@example.com",
+            password="password123",
+        )
+
+        self.assertEqual(registration.first_name, "John")
+        self.assertEqual(registration.last_name, "Doe")
+        self.assertEqual(registration.email, "johndoe@example.com")
+        self.assertEqual(registration.password, "password123")
+
+    def test_str_representation(self):
+        registration = Registration.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="johndoe@example.com",
+            password="password123",
+        )
+
+        self.assertEqual(str(registration), "John")
+
+if __name__ == "__main__":
+    unittest.main()

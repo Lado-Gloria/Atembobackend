8c60ff4e0dd59c4bb9c4f195f2f7c5079f3d5b1a
Lado-Gloria <glorialadoo@gmail.com>
2023-09-14 17:42:02 +0300

    changed timefield to datefield

--- atembobackend/location/models.py
+++ atembobackend/location/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Location(models.Model):
     region_name = models.CharField(max_length=255)
-    installation_date = models.DateTimeField(null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    installation_date = models.DateField(null=True, blank=True)
+    updated_at = models.DateField(auto_now=True)
     
 
     def __str__(self):

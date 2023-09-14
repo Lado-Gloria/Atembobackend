b28deeeb16195772db69b6c58b551cbbf2bc8979
Lado-Gloria <glorialadoo@gmail.com>
2023-09-14 23:40:59 +0300

    merged files

--- atembobackend/api/serializers.py
+++ atembobackend/api/serializers.py
@@ -1,4 +1,14 @@
 from rest_framework import serializers
+from location.models import Location
+
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Location
+        fields = "__all__"
+
+   
 from flowrate.models import Device, FlowRate
 
 class DeviceSerializer(serializers.ModelSerializer):

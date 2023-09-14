140e9094bbc31f5e273d064717f9f49aa738a228
Lado-Gloria <glorialadoo@gmail.com>
2023-09-14 20:52:42 +0300

    Created database variables

--- atembobackend/atembobackend/settings.py
+++ atembobackend/atembobackend/settings.py
@@ -62,14 +62,15 @@ WSGI_APPLICATION = 'atembobackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'atembo',
-        'USER': 'atembo',
-        'PASSWORD': 'atembo_pass',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
